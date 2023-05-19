eb06b571822d70efaec4a4e4da3b0826b418e458
KenoBuss <keno.buss@gmx.de>
2023-05-19 22:47:31 +0200

    -Add "oauth" in download 'cause of new pytube version. -some code cleaning

--- YT_Downloader.py
+++ YT_Downloader.py
@@ -1,54 +1,43 @@
 # CopyRight (c) 2023 Keno Buss
+# Version 1.2
 import os
 import sys
-from pytube import YouTube, Playlist
+from pytube import YouTube as YT, Playlist
 
+# #remove invalid chars
 def remove_invalid_chars(filename):
     invalid_chars = "<>:\"/\\|?*\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0b\x0c\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f"
     return "".join(char for char in filename if char not in invalid_chars)
 
-
-# mp3 download
-def download_video_as_mp3(url, save_path):
-    yt = YouTube(url)
-    audio = yt.streams.get_audio_only()
-    filename = remove_invalid_chars(audio.title) + ".mp3"
-    audio.download(save_path, filename=filename)
-
-def download_playlist_as_mp3(url, save_path, iterator):
+# download playlist
+def download_playlist(url, save_path, iterator, file_type):
     playlist = Playlist(url)
     video_urls = [video.embed_url for video in playlist.videos]
 
     for i, video_url in enumerate(video_urls, start=1):
-        download_video_as_mp3(video_url, save_path)
+        download_stream(video_url, save_path, file_type)
         print(f"URL: {iterator}: Audio {i} / {len(video_urls)} is downloaded.")
 
-# mp4 download
-def download_video_as_mp4(url, save_path):
-    yt = YouTube(url)
-    video = yt.streams.get_highest_resolution()
-    filename = remove_invalid_chars(video.title) + ".mp4"
-    video.download(save_path, filename=filename)
+# stream download
+def download_stream(url, save_path, file_type):
+    video = YT(url,use_oauth=True, allow_oauth_cache=True)
+    filename = remove_invalid_chars(video.title) + "." + file_type
+    
+    if file_type == "mp4":
+        stream = video.streams.get_highest_resolution()
+    elif file_type == "mp3":
+        stream = video.streams.get_audio_only()
+    
+    stream.download(output_path=save_path, filename=filename)
 
-def download_playlist_as_mp4(url, save_path, iterator):
-    playlist = Playlist(url)
-    video_urls = [video.embed_url for video in playlist.videos]
-
-    for i, video_url in enumerate(video_urls, start=1):
-        download_video_as_mp4(video_url, save_path)
-        print(f"URL: {iterator}: Video {i} / {len(video_urls)} is downloaded.")
-
-
-
-def check_if_single_video_or_playlist (url, path, format, iterator):
+# check is single stream or playlist
+def check_if_single_video_or_playlist (url, path, file_type, iterator):
     if url.startswith("https://www.youtube.com/watch?v=") or url.startswith("https://www.youtu.be/watch?v="):
-        if format == "mp3" : download_video_as_mp3(url, path)
-        else : download_video_as_mp4(url, path)
+        download_stream(url, path, file_type)
     elif url.startswith("https://www.youtube.com/playlist?list=") or url.starstwith("https://youtu.be/playlist?list="):
-        if format == "mp3" : download_playlist_as_mp3(url, path, iterator)
-        else : download_playlist_as_mp4(url, path, iterator)
+        download_playlist(url, path, iterator, file_type)
 
-
+# get urls
 def get_urls(filepath):
     with open(filepath, "r") as file:
         lines = file.readlines()
@@ -71,8 +60,8 @@ def main():
 
     # Prompt the user for the file format
     while True:
-        format = input("Do you want to download audio or video files? \n[mp3] audio [mp4] video : ").lower()
-        if format == "mp3" or format == "mp4":
+        file_type = input("Do you want to download audio or video files? \n[mp3] audio [mp4] video : ").lower()
+        if file_type == "mp3" or file_type == "mp4":
             break
         print("Invalid input!\nPlease enter 'mp3' for audio or 'mp4' for video.")
 
@@ -83,10 +72,17 @@ def main():
 
     for i, url in enumerate(urls, start=1):
         try:
-            check_if_single_video_or_playlist(url, save_path, format, i)
+            check_if_single_video_or_playlist(url, save_path, file_type, i)
             print(f"URL {i} / {len(urls)} are downloaded")
         except Exception as e:
             print(f"Something went wrong with URL {i} / {len(urls)}. Error: {e}", file=sys.stderr)
+    print("""
+====================================
+      All URLs are downloaded.
+      Thanks for using me! :)
+====================================
+    """)
 
 if __name__ == "__main__":
     main()
+
